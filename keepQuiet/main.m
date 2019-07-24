ff5cb27794be491ed3e426b61b2544fd87885081
digital-pers0n <aquaphonic@gmail.com>
2019-07-24 21:44:00 +0300

    Initial Commit

---
+++ keepQuiet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  keepQuiet
+//
+//  Created by Terminator on 7/24/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
