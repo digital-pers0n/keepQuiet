33d5466730c28fbe15a2a1741192e28b0859d0ba
digital-pers0n <aquaphonic@gmail.com>
2019-07-24 22:22:44 +0300

    Add application delegate class

---
+++ keepQuiet/KQAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  KQAppDelegate.h
+//  keepQuiet
+//
+//  Created by Terminator on 7/24/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+@import Cocoa;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KQAppDelegate : NSObject <NSApplicationDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ keepQuiet/KQAppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  KQAppDelegate.m
+//  keepQuiet
+//
+//  Created by Terminator on 7/24/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+#import "KQAppDelegate.h"
+
+@implementation KQAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notification {
+
+}
+
+@end
