92eafb2d3b31d7af4f2760754d0607283702ac56
digital-pers0n <aquaphonic@gmail.com>
2019-07-31 23:10:40 +0300

    Print program version

--- keepQuiet/main.m
+++ keepQuiet/main.m
@@ -8,8 +8,17 @@
 
 #import "KQAppDelegate.h"
 
+#define PROGRAM_NAME "keepQuiet"
+#define PROGRAM_VERSION "0.1"
+
 int main(int argc, const char * argv[]) {
-    
+    if (argc > 1) {
+        const char *arg  = argv[1];
+        if (strncmp(arg, "-v", 2) == 0) {
+            printf("%s %s\n", PROGRAM_NAME, PROGRAM_VERSION);
+            return 0;
+        };
+    }
     /* Global hot keys only work in accordance with Cocoa/Carbon applications */
     NSApplication *application = NSApplication.new;
     KQAppDelegate *delegate = KQAppDelegate.new;
